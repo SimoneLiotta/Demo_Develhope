e56b9ab9c899d683fe7ceea2fcc3721c3c8ef267
SimoneLiotta <simoneliotta99@gmail.com>
2023-10-25 16:35:44 +0200

    First commit

--- src/Esercizio4.java
+++ src/Esercizio4.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Esercizio4 {
+public class Esercizio4 {
+    public static void main(String[] args) {
+
+        char first = '@';
+        char second = '#';
+        int asciiSum = first + second;
+
+        System.out.println(asciiSum);
+    }
 }
