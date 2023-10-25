ab663f74cf617d57c74dd806122066024c8e5df2
SimoneLiotta <simoneliotta99@gmail.com>
2023-10-25 16:26:07 +0200

    esercizi

--- src/Esercizio1.java
+++ src/Esercizio1.java
@@ -1,6 +1,6 @@
 import java.net.StandardSocketOptions;
 
-public class Main {
+public class Esercizio1 {
     public static void main(String[] args) {
         /*int base = 10;
         int altezza = 12;
