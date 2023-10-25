7f9278251ed5d9090cb10354f0eab458c36036c7
SimoneLiotta <simoneliotta99@gmail.com>
2023-10-25 17:16:40 +0200

    prova reset

--- src/Esercizio1.java
+++ src/Esercizio1.java
@@ -2,12 +2,12 @@ import java.net.StandardSocketOptions;
 
 public class Esercizio1 {
     public static void main(String[] args) {
+
         int base = 10;
         int altezza = 12;
         int risultato = (base*altezza)/2;
 
         System.out.println("Area: " + risultato);
-
     }
 }
 
--- src/Esercizio4.java
+++ src/Esercizio4.java
@@ -1,10 +1,11 @@
 public class Esercizio4 {
     public static void main(String[] args) {
 
-        char first = '@';
+        char first = '[';
         char second = '#';
         int asciiSum = first + second;
 
         System.out.println(asciiSum);
+
     }
 }
