232c6dfba9ea8916f353d47455d2f49e44d9d8bf
SimoneLiotta <simoneliotta99@gmail.com>
2023-10-27 15:16:18 +0200

    aggiunta esercizi

---
+++ src/EsercizioCicliAvanzati2.java
@@ -0,0 +1,15 @@
+public class EsercizioCicliAvanzati2 {
+    public static void main(String[] args) {
+        int num = 8;
+        stampaNum(num);
+    }
+    public static void stampaNum (int num){
+        for (int i = 0;i<=num;i++){
+
+            if (i==5){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
