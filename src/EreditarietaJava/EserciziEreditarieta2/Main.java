b7fae37cffabfc3909b0f62bec5e375e997bb8e5
SimoneLiotta <simoneliotta99@gmail.com>
2023-11-07 16:31:07 +0100

    aggiungi esercizio

---
+++ src/EreditarietaJava/EserciziEreditarieta2/Main.java
@@ -0,0 +1,13 @@
+package EreditarietaJava.EserciziEreditarieta2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Rettangolo rettangolo = new Rettangolo("rettangolo", 3, 12);
+
+
+        rettangolo.calcolaArea();
+
+
+    }
+}
