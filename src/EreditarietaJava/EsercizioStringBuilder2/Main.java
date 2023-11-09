f5968ed421c4af659ad6a73b5d9d38036c34eee3
SimoneLiotta <simoneliotta99@gmail.com>
2023-11-09 17:45:41 +0100

    aggiunta esercizi

---
+++ src/EreditarietaJava/EsercizioStringBuilder2/Main.java
@@ -0,0 +1,24 @@
+package EreditarietaJava.EsercizioStringBuilder2;
+
+public class Main {
+    public static void main(String[] args) {
+        String testo = "Ma che bel castello marcondirondirondello";
+        int index = 2;
+        System.out.println(covertiInUnicode(testo,index));
+    }
+   public static String covertiInUnicode(String testo, int index) {
+
+       StringBuilder risultato = new StringBuilder("Il valore Unicode del carattere in posizione " + index);
+
+       if (index > testo.length()) {
+           risultato.replace(0, risultato.length(), "L'indice è fuori dai limiti della stringa");
+       } else {
+           char carattere = testo.charAt(index);
+           risultato.append(" \"" + carattere + "\" è " + Integer.toHexString(carattere));
+       }
+       return risultato.toString();
+    }
+}
+
+//ero indeciso se togliere gli spazi dalla stringa prima di cercare il carattere, ma visto che anche lo spazio
+//ha un valore unicode, l'ho lasciato cosi
