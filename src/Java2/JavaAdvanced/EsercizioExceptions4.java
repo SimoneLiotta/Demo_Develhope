dbe95c42a6fae9d22e264af31ceacfe2781a1f0e
SimoneLiotta <simoneliotta99@gmail.com>
2023-11-16 16:36:13 +0100

    modifica esercizi

--- src/Java2/JavaAdvanced/EsercizioExceptions4.java
+++ src/Java2/JavaAdvanced/EsercizioExceptions4.java
@@ -2,19 +2,18 @@ package Java2.JavaAdvanced;
 
 public class EsercizioExceptions4 {
     public static void main(String[] args) {
-        try {
-            System.out.println(dividiArray(5,2));
-        } catch (ArithmeticException e){
-            System.out.println(e.getMessage());
-        }
-
+        dividiArray(5);
     }
-    public static int dividiArray(int index, int num){
-        int [] numeri = {2,4,6,8,10};
-        if (num == 0 || index>numeri.length-1) {
-            throw new ArithmeticException("Errore");
-        } else {
-            return numeri[index]/num;
+
+    public static void dividiArray(int index) {
+        int[] numeri = {2, 4, 6, 8, 10};
+
+        try {
+            int risultato1 = numeri[index] / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("ArithmeticException: " + e.getMessage());
+        } catch (ArrayIndexOutOfBoundsException er) {
+            System.out.println("ArrayIndexOutOfBoundsException: " + er.getMessage());
         }
     }
 }
