412b6079087107c27f5578e4b5a76953ad525aa6
SimoneLiotta <simoneliotta99@gmail.com>
2023-11-23 11:29:07 +0100

    aggiunta esercizi

---
+++ src/Java2/ListMapRecord/EsercizioRecord1/Main.java
@@ -0,0 +1,10 @@
+package Java2.ListMapRecord.EsercizioRecord1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Punto punto = new Punto(3, 4);
+        System.out.println(punto);
+
+    }
+}
---
+++ src/Java2/ListMapRecord/EsercizioRecord1/Punto.java
@@ -0,0 +1,4 @@
+package Java2.ListMapRecord.EsercizioRecord1;
+
+public record Punto(int x, int y) {
+}
