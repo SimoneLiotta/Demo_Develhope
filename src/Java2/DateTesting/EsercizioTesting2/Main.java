62f89f4208ec7ad8b2550ab73bf6bdb1a54cd9dc
SimoneLiotta <simoneliotta99@gmail.com>
2023-11-28 14:22:13 +0100

    modifica esercizi

--- src/Java2/DateTesting/EsercizioTesting2/Main.java
+++ src/Java2/DateTesting/EsercizioTesting2/Main.java
@@ -7,20 +7,28 @@ import java.util.Date;
 
 public class Main {
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
 
         OffsetDateTime data = OffsetDateTime.parse("2002-03-01T13:00:00Z");
         System.out.println(data);
 
-        System.out.println("dataFormattata1: " + dataFormattata(data, FormatStyle.FULL));
-        System.out.println("dataFormattata2: " + dataFormattata(data, FormatStyle.MEDIUM));
-        System.out.println("dataFormattata3: " + dataFormattata(data, FormatStyle.SHORT));
+
+        System.out.println("dataFormattata1: " + fullFormattedData(data));
+        System.out.println("dataFormattata2: " + mediumFormattedData(data));
+        System.out.println("dataFormattata3: " + shortFormattedData(data));
 
     }
 
-    public String dataFormattata(OffsetDateTime data, FormatStyle pattern) {
-        String dataFormattata = data.format(DateTimeFormatter.ofLocalizedDate(pattern));
-        return dataFormattata;
+    public static String fullFormattedData(OffsetDateTime data) {
+        return data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL));
+
+    }
+    public static String mediumFormattedData(OffsetDateTime data) {
+        return data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM));
+
+    }
+    public static String shortFormattedData(OffsetDateTime data) {
+        return data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT));
 
     }
 }
--- src/Java2/DateTesting/EsercizioTesting2/MainTest.java
+++ src/Java2/DateTesting/EsercizioTesting2/MainTest.java
@@ -10,17 +10,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
     private final Main testing = new Main();
     OffsetDateTime data = OffsetDateTime.parse("2002-03-01T13:00:00Z");
+
     @Test
-    void dataFormattata() {
-        String result1 = testing.dataFormattata(data, FormatStyle.FULL);
+    void fullFormattedData() {
+        String result1 = testing.fullFormattedData(data);
         assertEquals("venerdì 1 marzo 2002", result1, "dovrebbe restituire una data in formato FULL");
-
-        String result2 = testing.dataFormattata(data, FormatStyle.MEDIUM);
-        assertEquals("1 mar 2002", result2, "dovrebbe restituire una data in formato MEDIUM");
-
-        String result3 = testing.dataFormattata(data, FormatStyle.SHORT);
-        assertEquals("01/03/02", result3, "dovrebbe restituire una data in formato SHORT");
-
     }
 
+    @Test
+    void mediumFormattedData() {
+        String result2 = testing.mediumFormattedData(data);
+        assertEquals("1 mar 2002", result2, "dovrebbe restituire una data in formato MEDIUM");
+    }
+
+    @Test
+    void shortFormattedData() {
+        String result3 = testing.shortFormattedData(data);
+        assertEquals("01/03/02", result3, "dovrebbe restituire una data in formato SHORT");
+    }
 }
