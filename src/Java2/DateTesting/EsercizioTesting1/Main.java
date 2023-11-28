ddf40e36d8815f2d9f6b3c6bef06d45ec1cedb44
SimoneLiotta <simoneliotta99@gmail.com>
2023-11-28 17:39:10 +0100

    aggiunta esercizi

--- src/Java2/DateTesting/EsercizioTesting1/Main.java
+++ src/Java2/DateTesting/EsercizioTesting1/Main.java
@@ -3,5 +3,15 @@ package Java2.DateTesting.EsercizioTesting1;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println(isInRange(null,1,5));
+
+    }
+    public static Boolean isInRange(Integer value, Integer min, Integer max) {
+        if (value==null || max==null || min==null){
+            return null;
+        } else {
+            return min < value && value < max;
+        }
+
     }
 }
--- src/Java2/DateTesting/EsercizioTesting1/MainTest.java
+++ src/Java2/DateTesting/EsercizioTesting1/MainTest.java
@@ -1,7 +1,26 @@
 package Java2.DateTesting.EsercizioTesting1;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+    private final Main testing = new Main();
+
+    @Test
+    void isInRangeTrue() {
+        boolean result = testing.isInRange(3,1,6);
+        assertEquals(true, result, "In questo caso dovrebbe restituire true");
+    }
+    @Test
+    void isInRangeFalse() {
+        boolean result = testing.isInRange(2,3,6);
+        assertEquals(false, result, "In questo caso dovrebbe restituire false");
+    }
+@Test
+    void isInRangeError() {
+        Boolean result = testing.isInRange(1,null,5);
+        assertEquals(null, result, "Se uno dei parametri è null, dovrebbe restituire null");
+    }
 
 }
